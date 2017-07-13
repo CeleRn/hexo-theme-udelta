78a8913516e1d0c427fb805dfdb9ebdbbdb67442
CeleRn <celern84@gmail.com>
2017-07-13 20:49:50 +0300

    first dev

--- assets/commons.js
+++ assets/commons.js
@@ -1,2 +1,10 @@
+// Загрузка основных стилей
 require("./styles/commons.scss");
+
+// Логотип
 require("./images/logo-udelta.svg");
+
+// Загрузка иконок для спрайта
+require('./icons/phone.svg');
+require('./icons/email.svg');
+require('./icons/place.svg');
